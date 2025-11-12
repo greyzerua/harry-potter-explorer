fe18f35aaba32d598048d4f2ff2fb205bf12120e
greyzerua <greyzerua@ukr.net>
2025-11-12 18:50:17 +0200

    Update project name

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,11 @@ import { HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 
+const basename = import.meta.env.PROD ? '/harry-potter-test' : '/'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter basename={import.meta.env.PROD ? '/harry-potter-test' : '/'}>
+    <HashRouter basename={basename}>
       <App />
     </HashRouter>
   </StrictMode>,
--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,7 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
- 
 export default defineConfig({
-  base:  process.env.NODE_ENV === 'production' ? '/harry-potter-test/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/harry-potter-test/' : '/',
   plugins: [react()],
-  resolve: {
-    alias: {
-      src: path.resolve(__dirname, 'src'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@constants': path.resolve(__dirname, 'src/constants'),
-    },
-  },
-});
+})
