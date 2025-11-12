6fbc22fe86d09f773a5f901d3af651d4d07ebfe1
greyzerua <greyzerua@ukr.net>
2025-11-12 19:29:39 +0200

    Restore base path in Vite configuration for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/harry-potter-task/",
   plugins: [react()],
+  base: "/harry-potter-task/",
 })
